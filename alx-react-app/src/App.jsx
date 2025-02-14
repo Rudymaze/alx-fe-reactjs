7e19b50294d93e8e3e29ae537cb578215a27e119
RUDYMAZE <njockrudiger4@gmail.com>
2025-02-14 22:00:06 +0200

    added a new component UserProfile and initiated my first props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,12 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Header />
       <MainContent />
