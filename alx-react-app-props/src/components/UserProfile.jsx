0d43c444a7132689c1feb1524000611fc438eef1
RUDYMAZE <njockrudiger4@gmail.com>
2025-02-23 22:52:18 +0200

    commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
 function UserProfile() {
-  return <div></div>;
+  const { userData } = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
