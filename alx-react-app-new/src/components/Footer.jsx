3a89b876351342cac1da4a137b40875683951477
RUDYMAZE <njockrudiger4@gmail.com>
2025-02-23 11:13:31 +0200

    dupliocated the prev react project, and enhanced all components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <span style={{ fontWeight: "bold" }}>© 2023</span> City Lovers
+      </p>
+    </footer>
+  );
+}
+export default Footer;
