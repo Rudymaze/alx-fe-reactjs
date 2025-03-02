5e9870bdec7a2016b5b417bbdb2b287d49e5ef55
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-02 10:14:18 +0200

    new commit

--- my-company/src/main.jsx
+++ my-company/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowsweRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
