4157ede8ff94bb64a5791ec7000502d9ec69957b
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-08 12:00:09 +0200

    queryClient added

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import PostsComponent from "./components/PostsComponent ";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <PostsComponent />
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
     </>
   );
 }
