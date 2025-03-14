1a4bc85e7b38beb87c3ca013d4cb3fed9d23c526
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-14 23:41:45 +0200

    commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="user-profile sm:p-4 sm:max-w-xs  md:p-8 md:max-w-sm  hover:shadow-xl hover:text-blue-500  bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg hover: scale-110 transition-transform duration-300 ease-in-out">
+    <div className="user-profile sm:p-4 sm:max-w-xs  md:p-8 md:max-w-sm  hover:shadow-xl hover:text-blue-500  bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover: scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg,text-sm md:text-xl,text-base">
         John Doe
