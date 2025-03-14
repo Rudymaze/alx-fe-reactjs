3ce808f51bcc1985ba2d3451d881e574e3b5e87f
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-14 22:16:13 +0200

    purge files added

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
