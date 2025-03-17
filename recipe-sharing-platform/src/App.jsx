6b70be6807acb894621741852a332355409117fe
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-17 22:10:55 +0200

    HomePage created and styled

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <HomePage />
     </>
   );
 }
