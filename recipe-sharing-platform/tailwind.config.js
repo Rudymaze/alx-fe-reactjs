b1be0b1649e7420ac9e23cb90605e4af9e8d7e6f
RUDYMAZE <njockrudiger4@gmail.com>
2025-03-17 21:24:11 +0200

    commits

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}  ./public/index.html"],
-  darkMode: false, // You can set it to 'media' or 'class' if needed"
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}  ",
+    "./public/index.html",
+  ],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+
   theme: {
     extend: {},
   },
